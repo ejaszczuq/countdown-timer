fc4c676e73163f31cbd9d736dd5f3d69b4690ba8
ejaszczuq <ewelina.jaszczuk13006@gmail.com>
2024-01-19 12:56:52 +0100

    for repair

--- src/app/Main/Main.tsx
+++ src/app/Main/Main.tsx
@@ -1,34 +1,45 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 import "./Main.scss";
 import NumberBox from "../../components/NumberBox/NumberBox";
 import NumberBoxTitle from "../../components/NumberBoxTitle/NumberBoxTitle";
 
-
 const MainPage = () => {
+  const [time, setTime] = useState({days: 0, hours: 0, minutes: 0, seconds: 0});
+
+  useEffect(() => {
+    const countdownDate = new Date("Feb 5, 2024 15:37:25").getTime();
+    const timer = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+      setTime({
+        days: Math.floor(distance / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+        minutes: Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60)),
+        seconds: Math.floor((distance % (1000 * 60)) / 1000)
+      });
+    }, 1000);
+    return () => clearInterval(timer);
+  }, []);
+
   return (
     <div className="main-page">
       <main>
         <div className="boxSection">
-          <NumberBox idProp="days" num={8} />
+          <NumberBox idProp="days" num={time.days} />
           <NumberBoxTitle content="days"/>
-          
         </div>
         <div className="boxSection">
-          <NumberBox idProp="hours" num={23} />
+          <NumberBox idProp="hours" num={time.hours} />
           <NumberBoxTitle content="hours"/>
-          
-      </div>
-      <div className="boxSection">
-          <NumberBox idProp="minutes" num={55} />
+        </div>
+        <div className="boxSection">
+          <NumberBox idProp="minutes" num={time.minutes} />
           <NumberBoxTitle content="minutes"/>
-          
-      </div>
-      <div className="boxSection">
-          <NumberBox idProp="seconds" num={41} />
+        </div>
+        <div className="boxSection">
+          <NumberBox idProp="seconds" num={time.seconds} />
           <NumberBoxTitle content="seconds"/>
-          
-      </div>
+        </div>
       </main>
       <footer>
         <div className="social-icons-container">
--- src/components/NumberBox/NumberBox.tsx
+++ src/components/NumberBox/NumberBox.tsx
@@ -1,5 +1,5 @@
-import React, { FC } from "react";
-
+import React, { FC, useEffect, useState } from "react";
+import { motion } from 'framer-motion';
 import "./NumberBox.scss";
 
 interface NumberBoxProps {
@@ -8,11 +8,34 @@ interface NumberBoxProps {
 }
 
 const NumberBox: FC<NumberBoxProps> = ({idProp, num}) => {
+    const [flip, setFlip] = useState(false);
+    const [prevNum, setPrevNum] = useState(num);
+
+    useEffect(() => {
+        setFlip(true);
+        const timer = setTimeout(() => {
+            setFlip(false);
+            setPrevNum(num);
+        }, 750); // Reset flip state 
+        return () => clearTimeout(timer);
+    }, [num]); // This will run every time `num` changes
+
     return (
-        
         <><div className="numberBox" id={idProp}>
-            <hr /><p>{num}</p><div className="circleRight" /><div className="circleLeft" />
-            </div></>
+            <div style={{ position: 'absolute' }} className="front">
+
+                <hr /><p>{num}</p><div className="circleRight" /><div className="circleLeft" />
+            </div>
+        <div style={{ position: 'absolute' }}>
+                <motion.div
+                    animate={{ rotateX: flip ? 0 : -90 }}
+                    transition={{ duration: 0.5 }} //1,2,4,7
+                    className="back"
+                >
+                    <hr /><p>{prevNum}</p><div className="circleRight" /><div className="circleLeft" />
+                </motion.div>
+            </div></div></>
+       
     );
 };
 
